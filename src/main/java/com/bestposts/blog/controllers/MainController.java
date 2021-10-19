3823466609806724c2b4f7a58a5c36622fe246c3
ch0c0-msk <general.lee121@yandex.ru>
2021-10-20 02:24:26 +0300

    Showing posts

--- src/main/java/com/bestposts/blog/controllers/MainController.java
+++ src/main/java/com/bestposts/blog/controllers/MainController.java
@@ -1,13 +1,22 @@
 package com.bestposts.blog.controllers;
 
+        import com.bestposts.blog.models.Posts;
+        import com.bestposts.blog.repos.PostsRepo;
+        import org.springframework.beans.factory.annotation.Autowired;
         import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
         import org.springframework.web.bind.annotation.GetMapping;
-        import java.util.Map;
 @Controller
 public class MainController {
 
-    @GetMapping()
-    public String Hello() {
+    @Autowired
+    private PostsRepo postsRepo;
+
+    @GetMapping("/main")
+    public String main(Model model) {
+
+        Iterable<Posts> posts = postsRepo.findAll();
+        model.addAttribute("posts",posts);
         return "main";
     }
 
