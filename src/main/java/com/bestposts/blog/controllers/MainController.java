390e20124abda7b56252039face04d1d0ac1e332
ch0c0-msk <general.lee121@yandex.ru>
2021-10-27 07:58:02 +0300

    Showoing full post

--- src/main/java/com/bestposts/blog/controllers/MainController.java
+++ src/main/java/com/bestposts/blog/controllers/MainController.java
@@ -6,8 +6,12 @@ package com.bestposts.blog.controllers;
         import org.springframework.stereotype.Controller;
         import org.springframework.ui.Model;
         import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.PathVariable;
         import org.springframework.web.bind.annotation.PostMapping;
         import org.springframework.web.bind.annotation.RequestParam;
+
+        import java.util.ArrayList;
+        import java.util.Optional;
 
 @Controller
 public class MainController {
@@ -35,5 +39,19 @@ public class MainController {
         Posts post = new Posts(label, postText);
         postsRepo.save(post);
         return "redirect:/main";
+    }
+
+    @GetMapping("/main/{id}")
+    public String showPost(@PathVariable(value = "id") Integer id, Model model) {
+        Optional<Posts> post = postsRepo.findById(id);
+        ArrayList<Posts> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post",res);
+        return "post-details";
+    }
+    @GetMapping("/login")
+    public String login() {
+
+        return "login";
     }
 }
