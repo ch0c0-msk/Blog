6da50c1eb98901c4f6b22b95098ad6e6fb926c61
ch0c0-msk <general.lee121@yandex.ru>
2021-12-07 00:56:53 +0300

    Authentication

--- src/main/java/com/bestposts/blog/repos/PostsRepo.java
+++ src/main/java/com/bestposts/blog/repos/PostsRepo.java
@@ -2,6 +2,5 @@ package com.bestposts.blog.repos;
 
 import org.springframework.data.repository.CrudRepository;
 import com.bestposts.blog.models.Posts;
-
 public interface PostsRepo extends CrudRepository<Posts, Integer> {
 }
